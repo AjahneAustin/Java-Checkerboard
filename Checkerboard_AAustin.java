3267506e729df7f9f2cb705a29c9c6f958d1aff6
MrFinesseyou <65263201+MrFinesseyou@users.noreply.github.com>
2022-02-03 16:25:35 -0500

    Add files via upload

---
+++ Checkerboard_AAustin.java
@@ -0,0 +1,35 @@
+import javax.swing.JApplet;
+import java.awt.*;
+import java.util.*;
+
+public class Checkerboard_AAustin extends JApplet {
+    
+    public void paint(Graphics g){
+        
+          int row;   // Row number, from 0 to 7
+          int col;   // Column number, from 0 to 7
+          int x,y;   // Top-left corner of square
+		  
+	         for ( row = 0;  row < 8;  row++ ) {
+          
+             for ( col = 0;  col < 8;  col++) {
+                x = col * 20;
+                y = row * 20;
+                if ( (row % 2) == (col % 2) )
+                   g.setColor(Color.red);
+                else
+                   g.setColor(Color.black);
+                g.fillRect(x, y, 20, 20);
+             } 
+          
+          } // end for row
+       
+       }  // end paint()
+
+    }  // end class
+
+      
+
+
+   
+
